bb547b3e07a743e2dd15bc69b03e9a721d126da6
moritz <moritz@q958.local>
2026-06-19 12:22:26 +0200

    chore: convert CRLF to LF locally

--- configuration.nix
+++
@@ -1,4 +0,0 @@
-# Kanonisch: nixos-rebuild switch --flake /etc/nixos#q958
-{ ... }: {
-  imports = [ ./machines/q958/default.nix ];
-}
--- packages/grok-cli/default.nix
+++
@@ -1,1 +0,0 @@
-{ stdenv, lib }: stdenv.mkDerivation { pname = "grok-cli"; version = "1.0"; src = ./.; installPhase = "mkdir -p $out/bin && echo 'echo dummy' > $out/bin/grok-cli && chmod +x $out/bin/grok-cli"; }
