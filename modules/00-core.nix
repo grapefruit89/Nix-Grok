ce7920042aa0fd04db9bd3c03450ba63119cc584
moritz <moritz@q958.local>
2026-06-19 13:04:43 +0200

    perf(core): implement automatic gc based on min-free and max-free storage thresholds

--- modules/00-core.nix
+++ modules/00-core.nix
@@ -223,6 +223,11 @@ in
           options = "--delete-older-than 7d";
           persistent = true;
         };
+
+        extraOptions = ''
+          min-free = ${toString (100 * 1024 * 1024)}
+          max-free = ${toString (1024 * 1024 * 1024)}
+        '';
       };
 
       environment.systemPackages = with pkgs; [
